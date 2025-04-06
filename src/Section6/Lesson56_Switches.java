48dd3071df83b96878e1ecb57ad59058eb1f75ee
Soufiane Kassouri <Soufiane.kassouri.2021@mumail.ie>
2025-04-06 15:20:21 +0100

    Section 6 Progress

--- src/Section6/Lesson56_Switches.java
+++ src/Section6/Lesson56_Switches.java
@@ -4,9 +4,9 @@ public class Lesson56_Switches {
 
     public static void main(String[] args) {
 
-        int switchValue = 3;
+        int switchValue = 5;
 
-        switch(switchValue){
+        switch (switchValue) {
             case 1:
                 System.out.println("Value is 1");
                 break;
@@ -17,7 +17,23 @@ public class Lesson56_Switches {
                 System.out.println("Value is 3");
                 break;
             default:
-                System.out.println("Value is " + switchValue);
+                System.out.println("Value is not 1, 2 or 3");
         }
+
+        System.out.println(getQuarter("APRIL"));
+    }
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + "is not a month";
+                yield badResponse;
+            }
+        };
+
     }
 }
---
+++ src/Section6/Lesson59_Challenge.java
@@ -0,0 +1,172 @@
+package Section6;
+
+public class Lesson59_Challenge {
+    public static void main(String[] args) {
+        String letter = "5";
+
+        switch(letter) {
+
+            case "A":
+                System.out.print(letter + " == ");
+                letter = "Able";
+                System.out.println(letter);
+                break;
+
+            case "B":
+                System.out.print(letter + " == ");
+                letter = "Baker";
+                System.out.println(letter);
+                break;
+
+            case "C":
+                System.out.print(letter + " == ");
+                letter = "Charlie";
+                System.out.print(letter);
+                break;
+
+            case "D":
+                System.out.print(letter + " == ");
+                letter = "Dog";
+                System.out.print(letter);
+                break;
+
+            case "E":
+                System.out.print(letter + " == ");
+                letter = "Easy";
+                System.out.print(letter);
+                break;
+
+            case "F":
+                System.out.print(letter + " == ");
+                letter = "Fox";
+                System.out.print(letter);
+                break;
+
+            case "G":
+                System.out.print(letter + " == ");
+                letter = "George";
+                System.out.print(letter);
+                break;
+
+            case "H":
+                System.out.print(letter + " == ");
+                letter = "How";
+                System.out.print(letter);
+                break;
+
+            case "I":
+                System.out.print(letter + " == ");
+                letter = "Item";
+                System.out.print(letter);
+                break;
+
+            case "J":
+                System.out.print(letter + " == ");
+                letter = "Jig";
+                System.out.print(letter);
+                break;
+
+            case "K":
+                System.out.print(letter + " == ");
+                letter = "King";
+                System.out.print(letter);
+                break;
+
+            case "L":
+                System.out.print(letter + " == ");
+                letter = "Love";
+                System.out.print(letter);
+                break;
+
+            case "M":
+                System.out.print(letter + " == ");
+                letter = "Mike";
+                System.out.print(letter);
+                break;
+
+            case "N":
+                System.out.print(letter + " == ");
+                letter = "Nan";
+                System.out.print(letter);
+                break;
+
+            case "O":
+                System.out.print(letter + " == ");
+                letter = "Oboe";
+                System.out.print(letter);
+                break;
+
+            case "P":
+                System.out.print(letter + " == ");
+                letter = "Peter";
+                System.out.print(letter);
+                break;
+
+            case "Q":
+                System.out.print(letter + " == ");
+                letter = "Queen";
+                System.out.print(letter);
+                break;
+
+            case "R":
+                System.out.print(letter + " == ");
+                letter = "Roger";
+                System.out.print(letter);
+                break;
+
+            case "S":
+                System.out.print(letter + " == ");
+                letter = "Sugar";
+                System.out.print(letter);
+                break;
+
+            case "T":
+                System.out.print(letter + " == ");
+                letter = "Tare";
+                System.out.print(letter);
+                break;
+
+            case "U":
+                System.out.print(letter + " == ");
+                letter = "Uncle";
+                System.out.print(letter);
+                break;
+
+            case "V":
+                System.out.print(letter + " == ");
+                letter = "Victor";
+                System.out.print(letter);
+                break;
+
+            case "W":
+                System.out.print(letter + " == ");
+                letter = "William";
+                System.out.print(letter);
+                break;
+
+            case "X":
+                System.out.print(letter + " == ");
+                letter = "X-Ray";
+                System.out.print(letter);
+                break;
+
+            case "Y":
+                System.out.print(letter + " == ");
+                letter = "Yoke";
+                System.out.print(letter);
+                break;
+
+            case "Z":
+                System.out.print(letter + " == ");
+                letter = "Zebra";
+                System.out.print(letter);
+                break;
+
+            default:
+                letter = letter + " was not found in the NATO Phonetic Alphabet";
+                System.out.print(letter);
+                break;
+        }
+
+    }
+}
